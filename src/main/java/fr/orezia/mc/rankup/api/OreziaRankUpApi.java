adb38b24ccea278d6d62e293eb31b825deafe2fd
dederobert <adinquer@yahoo.com>
2021-11-28 22:34:15 +0100

    Remove DAO factory references in OreziaRankUpApi

--- src/main/java/fr/orezia/mc/rankup/api/OreziaRankUpApi.java
+++ src/main/java/fr/orezia/mc/rankup/api/OreziaRankUpApi.java
@@ -2,17 +2,13 @@ package fr.orezia.mc.rankup.api;
 
 import fr.orezia.mc.core.api.OreziaPluginApi;
 import fr.orezia.mc.core.api.annotation.PublicApi;
-import fr.orezia.mc.rankup.api.dao.factory.DaoFactory;
 
 /**
- * Entry point of the plugin OreziaRankUp
+ * Entry point of the plugin OreziaRankUp.
  *
  * @since 1.0
  */
 @PublicApi
 public interface OreziaRankUpApi extends OreziaPluginApi {
-
-  @PublicApi
-  DaoFactory daoFactory();
 
 }
