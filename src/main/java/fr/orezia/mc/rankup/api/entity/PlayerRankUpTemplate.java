ed31bfd557f5677b073702a96a4d306cf958f4f9
Alexis DINQUER <adinquer@yahoo.com>
2021-11-30 21:49:32 +0100

    feat: Update entities & framework checker (#9)

--- src/main/java/fr/orezia/mc/rankup/api/entity/PlayerRankUpTemplate.java
+++ src/main/java/fr/orezia/mc/rankup/api/entity/PlayerRankUpTemplate.java
@@ -1,8 +1,12 @@
 package fr.orezia.mc.rankup.api.entity;
 
+import static java.util.Objects.requireNonNull;
+
 import fr.orezia.mc.core.api.annotation.PublicApi;
 import java.util.Map;
 import org.bukkit.configuration.serialization.SerializableAs;
+import org.checkerframework.common.returnsreceiver.qual.This;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,7 +18,10 @@ import org.jetbrains.annotations.Nullable;
  */
 @PublicApi
 @SerializableAs("PlayerRankUpTemplate")
-public class PlayerRankUpTemplate extends AbstractRankUpTemplate{
+public class PlayerRankUpTemplate extends AbstractRankUpTemplate {
+
+  private String masculineName;
+  private @Nullable String feminineName;
 
   /**
    * Default constructor.
@@ -31,5 +38,67 @@ public class PlayerRankUpTemplate extends AbstractRankUpTemplate{
   @PublicApi
   public PlayerRankUpTemplate(final @NotNull Map<@NotNull String, @Nullable Object> serialization) {
     super(serialization);
+    masculineName((String) requireNonNull(serialization.get("masculineName")));
+    feminineName((String) serialization.get("feminineName"));
   }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public @NotNull Map<@NotNull String, @Nullable Object> serialize() {
+    final Map<@NotNull String, @Nullable Object> serialization = super.serialize();
+    serialization.put("masculineName", masculineName());
+    serialization.put("feminineName", feminineName());
+    return serialization;
+  }
+
+  /**
+   * Gets the masculine name.
+   *
+   * @return the masculine name
+   */
+  @PublicApi
+  @Contract(pure = true)
+  public @NotNull String masculineName() {
+    return masculineName;
+  }
+
+  /**
+   * Sets the masculine name.
+   *
+   * @param masculineName the new masculine name
+   * @return {@code this}
+   */
+  @PublicApi
+  @Contract(value = "_ -> this", mutates = "this")
+  public @This @NotNull AbstractRankUpTemplate masculineName(final @NotNull String masculineName) {
+    this.masculineName = masculineName;
+    return this;
+  }
+
+  /**
+   * Gets the feminine name.
+   *
+   * @return the feminine name
+   */
+  @PublicApi
+  @Contract(pure = true)
+  public @Nullable String feminineName() {
+    return feminineName;
+  }
+
+  /**
+   * Sets the feminine name.
+   *
+   * @param feminineName the new feminine name
+   * @return {@code this}
+   */
+  @PublicApi
+  @Contract(value = "_ -> this", mutates = "this")
+  public @This @NotNull AbstractRankUpTemplate feminineName(final @Nullable String feminineName) {
+    this.feminineName = feminineName;
+    return this;
+  }
+
 }
